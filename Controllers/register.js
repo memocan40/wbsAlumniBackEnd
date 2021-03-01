cd33e5dca70c57ffc370ce51506b8e3f02f4ad89
olkhon <alexander.stapel@gmail.com>
2021-03-01 17:30:19 +0100

    comments

--- Controllers/register.js
+++ Controllers/register.js
@@ -1,37 +1,38 @@
-const pool = require("../db_config");
-let passwordHash = require('password-hash');
-let validator = require("node-email-validation");
 
+// dependencies
+const pool = require("../db_config");
+let passwordHash = require("password-hash");
+let validator = require("node-email-validation");
 
 module.exports = {
   newRegister: async (req, res) => {
+
+    // package applied for hashing pw
     let hashedPassword = passwordHash.generate(req.body.password);
-    const { name, email} = req.body;
+    const { name, email } = req.body;
+    // package applied checking for checking if email valid
     let validEmail = validator.is_email_valid(req.body.email);
-   if(validEmail) {
-    try {
-
-
-      const answerDB = await pool.query(
-        "INSERT INTO users (name, email, password) VALUES ( $1, $2, $3)",
-        [name, email, hashedPassword]
-      );
-      res.json({
-        message:
-          "New user with the following values:" +
-          [name, email, hashedPassword] + "has been created",
-        code: 200,
-        data: answerDB.rows,
-      });
-    } catch (e) {
-      console.log(e);
-      res.sendStatus(404);
+    if (validEmail) {
+      try {
+        const answerDB = await pool.query(
+          "INSERT INTO users (name, email, password) VALUES ( $1, $2, $3)",
+          [name, email, hashedPassword]
+        );
+        res.json({
+          message:
+            "New user with the following values:" +
+            [name, email, hashedPassword] +
+            "has been created",
+          code: 200,
+          data: answerDB.rows,
+        });
+      } catch (e) {
+        console.log(e);
+        res.sendStatus(404);
+      }
     }
-   }{
-     console.log('Please provide a valid email');
-   }
-
+    {
+      console.log("Please provide a valid email");
+    }
   },
-
-
 };
