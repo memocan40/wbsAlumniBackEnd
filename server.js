07f82126bea96086bfd2dce63a656826357fed14
olkhon <alexander.stapel@gmail.com>
2021-03-01 11:39:19 +0100

    working server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+//const pool = require("./dbconfig");
+
+const userRoutes = require("./Routes/users");
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// app.use("/users", userRoutes);
+
+
+// const { PORT } = process.env;
+
+app.listen(3000, () => console.log(`Server running on port: 3000 `));
