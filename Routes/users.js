eae13f26ef5dcf2ec0137791f311295f9831c0fd
Elie <eliegebransoued@gmail.com>
2021-03-03 19:54:36 +0100

    changing the post endpoint to users/register

--- Routes/users.js
+++ Routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {getUsers,newUser, getUserById, updateUser, deleteUser} = require("../Controllers/users");
 
 router.get("/", getUsers);
-router.post("/create", newUser);
+router.post("/register", newUser);
 router.get("/:id", getUserById);
 router.put("/update/:id", updateUser);
 router.delete("/delete/:id", deleteUser);
