07dece4ac37e66bb489ebd84b45c334e1e2cfb83
olkhon <alexander.stapel@gmail.com>
2021-03-04 12:39:08 +0100

    some login logic

--- Routes/users.js
+++ Routes/users.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {getUsers,newUser, getUserById, updateUser, deleteUser} = require("../Controllers/users");
+const {getUsers,newUser, getUserById, updateUser, deleteUser, loggedInUser} = require("../Controllers/users");
+const {loginUser} = require("../Controllers/login");
 
 router.get("/", getUsers);
 router.post("/", newUser);
+router.post("/login", loginUser);
+router.get("/dashboard", loggedInUser);
 router.get("/:id", getUserById);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
--- server.js
+++ server.js
@@ -4,17 +4,19 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const session = require('express-session');
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(session({secret:"123456789", resave:false, saveUninitialized: true}))
 //const pool = require("./dbconfig");
 
 const userRoutes = require("./Routes/users");
 const registerRoutes = require("./Routes/register");
 
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 
 app.use("/users", userRoutes);
 app.use("/register", registerRoutes);
