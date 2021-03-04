2eda24cfe7df56e362d38ec99d0d4777705e3408
Elie <eliegebransoued@gmail.com>
2021-03-04 10:13:14 +0100

    adding the ||3000 on Port

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.get("/", async (_, res) => {
   });
 
 
-const { PORT } = process.env;
+const { PORT } = process.env || 3000;
 
-app.listen(PORT || 3000 , () => console.log(`Server running on port: PORT `));
+app.listen(PORT, () => console.log(`Server running on port: PORT `));
