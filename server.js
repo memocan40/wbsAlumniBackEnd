50e8b3a051094fc81df9062eb117aefba8a51a89
memo40 <memocann4040@gmail.com>
2021-03-08 11:30:25 +0100

    chat

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const io = require('socket.io');
+
+//io.on('connection', (socket) => {
+ // console.log('a user connected');
+ // socket.on('disconnect', () => {
+  //  console.log('user disconnected');
+ // });
+//});
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
 
 
 const userRoutes = require("./Routes/users");
