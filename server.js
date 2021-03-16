c2bbf91d26617c165cd2ee7d81c32fbedb66656e
memo40 <memocann4040@gmail.com>
2021-03-16 15:09:58 +0100

    https

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.use("/images", express.static("uploads"));
 
 //Socket io
 //create and connect to chat server(socket.io)
-const http = require('http').Server(app);
+const http = require('https').Server(app);
 const io = require('socket.io')(http,{
   cors: {
     origin: "http://localhost:3000",
