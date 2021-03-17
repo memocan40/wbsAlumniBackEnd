87d537b1b7ae35a9faa25af007f341d081718f14
olkhon <alexander.stapel@gmail.com>
2021-03-17 20:15:22 +0100

    test mail

--- Controllers/users.js
+++ Controllers/users.js
@@ -68,7 +68,7 @@ module.exports = {
               "<br />" +
               "In order to use our plattform you have to verify your acount stay in touch!" +
               "< br />" +
-              `<a href='https://hidden-shelf-31461.herokuapp.com/users/register/confirm/${answerDB.rows[0].id}'><a/>`, // html body
+              `<a href='https://hidden-shelf-31461.herokuapp.com/users/register/confirm/'><a/>`, // html body
           });
 
           console.log("Message sent: %s", info.messageId);
